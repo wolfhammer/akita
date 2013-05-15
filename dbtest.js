9f015f5805ddc40a3880af22eced221d2226eed2
River Grimm <river.grimm@gmail.com>
2013-05-15 11:21:34 -0700

    Test script now uses env vars

--- dbtest.js
+++ dbtest.js
@@ -1,13 +1,23 @@
+// Simple script to verify we can connect to postgres.
+// After setting up config/config.development.js:
+// # node dbtest.js
+// Output: Tue Jan 15 2013 19:12:47 GMT-600 (CST)
+
 var pg = require('pg');
 var conf = require('config');
 
-var conString = "postgres://postgres:bptdev@localhost/bpt";
+var pgUser = conf.postgres.user;
+var pgPass = conf.postgres.pass;
+var pgHost = conf.postgres.host;
+var pgDb = conf.postgres.db;
+
+var conString = 'postgres://' + pgUser  + ':' + pgPass + '@' + pgHost '/' db;
 var client = new pg.Client(conString);
 client.connect(function(err) {
 	if (err) throw err;
   client.query('SELECT NOW() AS "theTime"', function(err, result) {
   		if (err) throw err;
       console.log(result.rows[0].theTime);
-      //output: Tue Jan 15 2013 19:12:47 GMT-600 (CST)
+      //
   })
 });
